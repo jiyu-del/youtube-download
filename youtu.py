f09b41bcf5f8cd47ff646485b8b4a061861342b6
jiyu-del <79136552+jiyu-del@users.noreply.github.com>
2023-09-08 15:43:01 +0800

    Update youtu.py
    add some description coments

--- youtu.py
+++ youtu.py
@@ -3,10 +3,13 @@ from yt_dlp import YoutubeDL
 import ffmpeg
 
 path = r'C:\Users\Administrator\Desktop\store\ffmpegc\ppl\bin'
+#Replace with your ffmpeg address
 os.environ['PATH'] +=  '' if path in os.environ['PATH'] else ';' + path
 option = {
+    # Replace "outtmpl" with the address where you want to save the output file
     "outtmpl": r'C:\Users\Administrator\Desktop\%(title)s.%(ext)s',
     "format":'bestvideo+bestaudio/best'
 }
 ydl = YoutubeDL(option)
+#Use URL that you want to download instead of "https://www.youtube.com/watch?v=1fCKnAmiXtU"
 result = ydl.download(['https://www.youtube.com/watch?v=1fCKnAmiXtU'])
